6db712f51524a4ce10b77bcf797239ac87cd826a
grant <grant@iowntheinter.net>
2016-08-09 17:15:26 -0700

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export dst=$(mktemp -d)
 grab_it() {
-  git clone https://$GH_USR:$GH_TOKEN@github.com/$GH_REPO $dst
+  git clone https://$GH_USER:$GH_TOKEN@github.com/$GH_REPO $dst
 }
 
 build_it() { #the publish destinations depend on the credentials in the enviornment
