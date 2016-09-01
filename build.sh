7c9c331cd81ed3f252a2bf427255c022a4254649
grant@iowntheinter.net <grant@iowntheinter.net>
2016-08-31 21:14:59 -0700

    fixup

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-export dst=$(mktemp -d)
+export dst="/tmp/$(echo $GH_REPO | cut -d '/' -f 2)"
 grab_it() {
-  git clone https://$GH_USER:$GH_TOKEN@github.com/$GH_REPO $dst
+  cd /tmp
+  git clone https://$GH_USER:$GH_TOKEN@github.com/$GH_REPO
 }
 
 build_it() { #the publish destinations depend on the credentials in the enviornment
