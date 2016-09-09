973c802c0226c4bc2bb33514cfb9e84958b86377
grant@iowntheinter.net <grant@iowntheinter.net>
2016-09-08 20:58:51 -0700

    these got moved

--- build.sh
+++ build.sh
@@ -30,23 +30,3 @@ release_it(){
     return -1
   }
 }
-docker_add(){
-  if [[ -f add.sh ]]; then
-  {
-    source add.sh
-    ADD $EXTRA
-   } || {
-    fail
-   }
-  fi
-}
-docker_build(){
-  cd $origdir
-  log "buildjob ${!buildjob}"
-  TS="docker build $BUILDARGS -t $VENDOR/$buildjob -f ${!buildjob} ."
-  log $TS
-  eval $TS
-}
-docker_clean(){
-  if [[ -f add.sh ]]; then bash add.sh clean; fi
-}
